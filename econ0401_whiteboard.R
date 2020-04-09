6fcce0f215d4d0fa332b67d03b02589b2a2a8eac
Chica Nerissa Morrow <chicamorrow@gmail.com>
2020-04-09 15:22:07 -0700

    Read in national percentile outcomes and tested first lm().

--- econ0401_whiteboard.R
+++ econ0401_whiteboard.R
@@ -1,1 +1,23 @@
 # R script where we can test all of our functions/manipulate data before pasting in Shinyapp
+
+
+# libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(readr)
+
+
+# data --------------------------------------------------------------------
+
+percentiles.data <- read_csv('national_percentile_outcomes.csv')
+percentiles <- percentiles.data
+
+
+
+# models ------------------------------------------------------------------
+
+
+percentile.model1 <- lm(kfr_pooled_pooled ~ par_pctile,
+                        data = percentiles)
+
+summary(percentile.model1)
