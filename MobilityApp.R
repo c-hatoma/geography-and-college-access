c3df08473583827e55c2f03000109415a7cdc4b3
Chica Nerissa Morrow <chicamorrow@gmail.com>
2020-05-04 23:31:45 -0700

    Map isn't rendering or at least is slow to render, some of the output table images keep disappearing from my local files (?), but mostly there.

--- MobilityApp.R
+++ MobilityApp.R
@@ -2,21 +2,19 @@
 
 library(shiny)
 library(tidyverse)
-library(readr)
+# library(readr)
 library(rvest)
 library(stats)
 library(readxl)
 library(dplyr)
-library(stringr)
+# library(stringr)
 library(ggplot2)
 library(ggthemes)
-library(stringr)
-library(data.table)
 library(geojsonio)
 library(leaflet)
-library(rgdal)
-library(haven)
-
+# library(rgdal)
+# library(haven)
+library(ggimage)
 
 
 # read data ---------------------------------------------------------------
@@ -28,14 +26,13 @@ cz.geo <- geojson_read("cz_geo.geojson",
 
 cz <- read_dta('cz.dta')
 
-
+tables <- read_csv('outputtables.csv')
 
 # Create input choices ----------------------------------------------------
 
-mapinputs <- c("Number of Colleges", "Number of Elite Colleges", 
-                "Number of Public 2-year and 4-year Colleges")
-mapmatches <- c("ncollege", "nelite", "npub")
-mapchoices <- data.frame(mapinputs, mapmatches)
+
+
+
 
 # App ---------------------------------------------------------------------
 
@@ -45,58 +42,106 @@ ui <- fluidPage(
   mainPanel(
     tabsetPanel(
       tabPanel("Geographic Analysis",
-               selectInput(inputId = "input1",
-                           label = "Select a Topic to Map",
-                           choices = mapinputs),
+               radioButtons("map", "Select a Topic to Map:",
+                            c("Number of Colleges in Commuting Zone", "Other")),
                leafletOutput(outputId = "map1", width = "150%")), 
       tabPanel("Regression Outputs", 
-               dataTableOutput(outputId = "table1")),
-      tabPanel("Analysis",
-               fluidRow(
-                 h4("What do we expect to happen when rent controls are implemented?"),
-                 p('Rent control can also lead to "mis-match" between tenants and rental units. 
-                             Once a tenant has secured a rent-controlled apartment, 
-                             he may not choose to move in the future and give up his rent control, 
-                             even if his housing needs change 
-                             (Suen 1980, Glaeser and Luttmer 2003, Sims 2011, Bulow and Klemperer 2012).')))
-  )
+               selectInput(inputId = "imagename",
+                           label = "Select a Regression Output",
+                           choices = tables$tablename),
+               imageOutput(outputId = "image1")),
+      tabPanel("About",
+        mainPanel(
+        h2("About the Project"),
+        p("This the final project for Professor Peter Hans Matthews' ECON401: Inequality and Justice Seminar at Middlebury College. 
+                         We were interested in looking at the relationship between inequality, social mobility, and higher education."),
+        br(),
+        h2("About Us"),
+        p("Chica Morrow and Ivy Yang are two senior economics majors at Middlebury College. 
+                         Chica likes trains and urban planning. Ivy likes the smell of rain and ice cream."),
+        br(),
+        h2("References"),
+        p("Chetty, Raj, et. al. All Outcomes by Commuting Zone, Race, Gender and Parental Income Percentile. Opportunity Insights. 2018."),
+        a("https://opportunityinsights.org/data/?geographic_level=0&topic=0&paper_id=1652#resource-listing"),
+        p("Chetty, Raj, et. al. Mobility Report Cards: The Role of Colleges in Intergenerational Mobility. Opportunity Insights. 2017."),
+        a("https://opportunityinsights.org/data/?geographic_level=0&topic=0&paper_id=536#resource-listing"),
+        p("Currie, Janet and Enrico Moretti. Mother's Education and the Intergenerational Transmission of Human Capital: Evidence from College Openings. The Quarterly Journal of Economics, Volume 118, Issue 4, November 2003. Pages 1495–1532. https://doi.org/10.1162/003355303322552856"),
+        p("Greenstone, Michael, Adam Looney, Jeremey Pastashnik, and Muxin Yu. “Thirteen Economic Facts about Social Mobility and the Role of Education.” The Brookings Institution. June 26, 2013."),
+        a("https://www.brookings.edu/research/thirteen-economic-facts-about-social-mobility-and-the-role-of-education/#cancel"),
+        p("Wilkinson, Richard and Kate Pickett. The Spirit Level. New York: Bloomsbury Press, 2009."),
+        
+        br(),
+        h2("Acknowledgements"),
+        p("We would like to thank Professor Matthews for his support and guidance throughout this project and during the entire semester.")))
   
-))
+)))
 
 server <- function(input, output) {
   
   output$map1 <- renderLeaflet({
     
-    title1 <- input$input1
+    if (input$map == "Number of Colleges") {
+      
+      bins1 <- c(1, 5, 10, 20, 40, 60, 82)
+      
+      colors1 <- colorBin(bins = bins1,
+                          palette = "YlOrRd",
+                          domain = cz.geo@data$ncollege)
+      
+      cz.geo %>%
+        leaflet() %>%
+        addTiles() %>%
+        addPolygons(fillColor = ~colors1(cz.geo@data$ncollege),
+                    weight = 1,
+                    color = "white",
+                    opacity = 0.5,
+                    fillOpacity = .7) %>%
+        setView(-96, 37.8, 3) %>%
+        addLegend(pal = colors1,
+                  values = cz.geo@data$ncollege,
+                  title = "Number of Colleges in Commuting Zones Across the US")
+      
+    } else if (input$map == "Other") {
+      
+      bins2 <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.59392)
+      
+      colors2 <- colorBin(bins = bins2,
+                          palette = "YlOrRd",
+                          domain = cz.geo@data$`Income Age 26 Slope`)
+      
+      cz.geo %>%
+        leaflet() %>%
+        addTiles() %>%
+        addPolygons(fillColor = ~colors2(cz.geo@data$`Income Age 26 Slope`),
+                    weight = 1,
+                    color = "white",
+                    opacity = 0.5,
+                    fillOpacity = .7) %>%
+        setView(-96, 37.8, 3) %>%
+        addLegend(pal = colors2,
+                  values = cz.geo@data$`Income Age 26 Slope`,
+                  title = "Slope of Income at Age 26 in Commuting Zones Across the US")
+      
+      }
+    })
+  
+  output$image1 <- renderImage({
     
-    mapchoice <- mapchoices %>%
-      filter(mapinputs == input$input1)
+    if (is.null(input$imagename))
+    return(NULL)
     
-    mapvar <- mapchoice[2]
+    printtable <- tables %>%
+      filter(tablename == input$imagename)
     
-    bins1 <- c(1, 5, 10, 20, 40, 60, 90)
+    printpath <- paste(printtable[2])
     
-    colors1 <- colorBin(bins = bins1,
-                        palette = "YlOrRd",
-                        domain = cz.geo@data$ncollege)
-    
-    cz.geo %>%
-      leaflet() %>%
-      addTiles() %>%
-      addPolygons(fillColor = ~colors1(cz.geo@data$mapvar),
-                  weight = 1,
-                  color = "white",
-                  opacity = 0.5,
-                  fillOpacity = .7) %>%
-      setView(-96, 37.8, 3) %>%
-      addLegend(pal = colors1,
-                values = cz.geo@data$mapvar,
-                title = input1)
+    list(src = printpath,
+         contentType = 'image/png',
+         width = 900,
+         height = 500,
+         alt = "This is alternate text")
     
   })
-  
-  output$table1 <- renderTable(txttable.nelite)
-  
 }
 
 shinyApp(ui, server)
