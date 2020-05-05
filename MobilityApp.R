79f56fbd9552b5a9c0d6a08e01102397c6d0b1b0
Chica Nerissa Morrow <chicamorrow@gmail.com>
2020-05-04 17:27:01 -0700

    Added leaflet and nelite table (table unsuccessful).

--- MobilityApp.R
+++ MobilityApp.R
@@ -24,34 +24,34 @@ library(haven)
 
 # read data ---------------------------------------------------------------
 
-cz.mobility <- read_csv('cz.mobility.csv')
-
-cz.geojson <- geojson_read("cz1990.json",
-                           what = "sp")
-
-cz.conversions <- read_xls('cz00_eqv_v1.xls')
+# cz.mobility <- read_csv('cz.mobility.csv')
+# 
+# cz.geojson <- geojson_read("cz1990.json",
+#                            what = "sp")
+# 
+# cz.conversions <- read_xls('cz00_eqv_v1.xls')
 
 
 
 # Converting geo cz1990 to cz2000 and adding cz to geo --------------------
 
-cz.conversions <- cz.conversions[, c(2:4)]
-cz.conversions$`Commuting Zone ID, 1990` <- as.numeric(cz.conversions$`Commuting Zone ID, 1990`)
-cz.conversions$`Commuting Zone ID, 1980` <- as.numeric(cz.conversions$`Commuting Zone ID, 1980`)
-colnames(cz.conversions)[2] <- 'cz1990'
-colnames(cz.conversions)[1] <- 'cz2000'
-colnames(cz.conversions)[3] <- 'cz1980'
-
-head(cz.geojson@data)
-cz.geo <- cz.geojson
-colnames(cz.mobility)[1] <- 'cz2000'
-
-cz.geo@data <- left_join(cz.geo@data,
-                         cz.conversions[, -3],
-                         by = c('cz' = 'cz1990'))
-cz.geo@data <- left_join(cz.geo@data,
-                         cz.mobility,
-                         by = 'cz2000')
+# cz.conversions <- cz.conversions[, c(2:4)]
+# cz.conversions$`Commuting Zone ID, 1990` <- as.numeric(cz.conversions$`Commuting Zone ID, 1990`)
+# cz.conversions$`Commuting Zone ID, 1980` <- as.numeric(cz.conversions$`Commuting Zone ID, 1980`)
+# colnames(cz.conversions)[2] <- 'cz1990'
+# colnames(cz.conversions)[1] <- 'cz2000'
+# colnames(cz.conversions)[3] <- 'cz1980'
+# 
+# head(cz.geojson@data)
+# cz.geo <- cz.geojson
+# colnames(cz.mobility)[1] <- 'cz2000'
+# 
+# cz.geo@data <- left_join(cz.geo@data,
+#                          cz.conversions[, -3],
+#                          by = c('cz' = 'cz1990'))
+# cz.geo@data <- left_join(cz.geo@data,
+#                          cz.mobility,
+#                          by = 'cz2000')
 
 
 
@@ -63,12 +63,27 @@ ui <- fluidPage(
       tabPanel("Tab1", 
                leafletOutput(outputId = "map1", width = "150%")), 
       tabPanel("Tab2", 
-               leafletOutput(outputId = "map2", width = "150%"))),
+               dataTableOutput(outputId = "table1"))),
   )
   
 )
 
-server <- function(input, output, session) {
+server <- function(input, output) {
+  
+  output$map1 <- renderLeaflet({
+    
+    cz.geojson <- geojson_read("cz1990.json",
+                               what = "sp")
+    # View(cz.geojson@data)
+    cz.geojson %>%
+      leaflet() %>%
+      #addTiles() %>%
+      addPolygons() %>%
+      setView(-96, 37.8, 3)
+    
+  })
+  
+  output$table1 <- renderTable(txttable.nelite)
   
 }
 
